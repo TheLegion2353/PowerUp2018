08e081bff4054e0adc61e0e9c965224dd1bbc23a
Byte11 <shirmak11@gmail.com>
2018-01-10 16:44:43 -0600

    First Commit. Has working chassis code for mecanum drive (hasn't been tested much).

---
+++ PowerUp2018/src/org/usfirst/frc/team2353/robot/RobotMap.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team2353.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	//USB Ports
+	public final static int xboxPort = 0; 
+		
+	//Motors
+	public final static int frontLeftMotor = 4, rearLeftMotor = 6, frontRightMotor = 5, rearRightMotor = 7;
+	
+	//Black: Roller
+	//Red: Winch Left
+}
---
+++ PowerUp2018/src/org/usfirst/frc/team2353/robot/subsystems/Chassis.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team2353.robot.subsystems;
+
+
+import org.usfirst.frc.team2353.robot.RobotMap;
+import org.usfirst.frc.team2353.robot.commands.MecanumDrive;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+*
+*/
+public class Chassis extends Subsystem {
+	private Victor frontLeftMotor, rearLeftMotor,frontRightMotor, rearRightMotor;
+	private static RobotDrive drive;
+
+	public Chassis() {
+		frontLeftMotor = new Victor(RobotMap.frontLeftMotor);
+		rearLeftMotor = new Victor(RobotMap.rearLeftMotor);
+		frontRightMotor = new Victor(RobotMap.frontRightMotor);
+		rearRightMotor = new Victor(RobotMap.rearRightMotor);
+   	drive = new RobotDrive(frontLeftMotor, rearLeftMotor,frontRightMotor, rearRightMotor);
+   	
+   	drive.setInvertedMotor(MotorType.kFrontLeft, true);
+   	drive.setInvertedMotor(MotorType.kRearLeft, true);
+	}
+
+   public void initDefaultCommand() {
+   	setDefaultCommand(new MecanumDrive());
+   }
+   
+   public void mecanumDrive_Cartesian(double x, double y, double rotation, double gyro) {
+       drive.mecanumDrive_Cartesian(x, y, rotation, gyro);
+   }
+}
+
