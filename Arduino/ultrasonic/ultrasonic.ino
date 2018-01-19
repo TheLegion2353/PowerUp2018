02374ddde6c7020e0d1c02b108f13e28731ecf98
r2awes <r2awes@gmail.com>
2018-01-18 22:08:18 -0600

    Ultrasonic Sensor Working
    Ultrasonic sensor works with the arduino now and will send updates through the arduino console. Sending over I2C is untested as of yet but should still work once it is set up on the Roborio

---
+++ Arduino/ultrasonic/ultrasonic.ino
@@ -0,0 +1,46 @@
+/*
+Name: Ultrasonic Setup and Arduino to Roborio Communication
+Author: Richard McHorgh (http://mchorghwebdesign.com)
+Started: 18 Jan 2018
+*/
+#include "Wire.h"
+
+const int anPin = 0;
+long anVolt, mm, inches, feet;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void read_sensor(){
+  anVolt = analogRead(anPin);
+  mm = anVolt * 5;
+  inches = mm/25.4;
+  feet = inches*12;
+}
+
+void print_range(){
+  Serial.print(mm);
+  Serial.print("mm");
+  Serial.print(inches);
+  Serial.print("inches away");
+  Serial.print(feet);
+  Serial.println("feet away");
+}
+
+void transmit(){
+  Wire.beginTransmission(2);
+  Wire.write("Distance: ");
+  Wire.write(mm);
+  Wire.write("mm");
+  Wire.endTransmission();
+
+  delay(500);
+}
+
+void loop() {
+  read_sensor();
+  print_range();
+  delay(100);
+  transmit();
+}
