394de1507db9c511641c8db340b6a6b8548901d6
Byte11 <shirmak11@gmail.com>
2018-01-24 17:08:38 -0600

    Arduino works over USB using serial

--- PowerUp2018/src/org/usfirst/frc/team2353/robot/commands/UltrasonicRead.java
+++ PowerUp2018/src/org/usfirst/frc/team2353/robot/commands/UltrasonicRead.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team2353.robot.commands;
 
+import java.nio.ByteBuffer;
+
 import org.usfirst.frc.team2353.robot.OI;
 import org.usfirst.frc.team2353.robot.Robot;
-import org.usfirst.frc.team2353.robot.SerialPortInterface;
 
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -11,18 +13,38 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class UltrasonicRead extends Command {
 
+	private static SerialPort serialPort;
+	int BaudRate = 9600;
+	int databits = 8;
+	
     public UltrasonicRead() {
        requires(Robot.ultrasonic);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	//serialPort = new SerialPort(BaudRate, SerialPort.Port.kUSB, databits);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	if(OI.xboxController.getRawButton(OI.AButtonNum) == true) {
-    		//Robot.ultrasonic.printSerial(SerialPortInterface.Read());
+    		byte[] bytes = new byte[4];
+    		ByteBuffer.wrap(bytes).putInt(1);
+    		
+    		System.out.println(bytes.toString());
+    		
+    		serialPort.write(bytes, bytes.length);
+    		
+    		try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		
+    		String in = serialPort.readString();
+    		System.out.println("Distance: " + in);
 		}
     }
 
